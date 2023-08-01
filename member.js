953f942003d501e4c792c615303527dcd97f90cd
J. Miguel Medina <79060767+JMMorph@users.noreply.github.com>
2023-08-01 06:30:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.backgroundColor = "#3d3d3d";
+    skills.style.backgroundColor = "#2d2d2d";
+    projects.style.backgroundColor = "#2d2d2d";
+    contact.style.backgroundColor = "#2d2d2d";
+    about.style.backgroundColor = "#2d2d2d";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
+ 
